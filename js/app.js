7d90f151eb5c23c164a3d3e242e8f15dba49c40e
Jon Holmberg <Jon Holmberg>
2016-09-19 20:16:28 -0700

    preferences objects defined

--- js/app.js
+++ js/app.js
@@ -1,68 +1,105 @@
 var strong=false;
-var tang=false;
+var salty=false;
+var bitter=false;
 
 	var Bartender = function() {
-    $("bartender-question").append().html("<p>Do ye like yer drinks strong?</p>");
-	$("bartender-question").append().html("<span class='flow-text'><a class='waves-effect waves-light btn yes-strong'>Yes</a><a class='waves-effect waves-light btn no-strong'>No</a></span>");
+    $(".bartender-question").append().html("<p>Do ye like yer drinks strong?</p>");
+	$(".bartender-question").append()("<span class='flow-text'><a class='waves-effect waves-light btn yes-strong'>Yes</a><a class='waves-effect waves-light btn no-strong'>No</a></span>");
 	$(".yes-strong").click(function(){
 		strong = true;		
 	});
-	$("bartender-question").html("");
-	$("bartender-question").append().html("<p>Do ye like it with a salty tang?</p>");
-	$("bartender-question").append().html("<span class='flow-text'><a class='waves-effect waves-light btn yes-tang'>Yes</a><a class='waves-effect waves-light btn no-tang'>No</a></span>"
+	$(".bartender-question").html("");
+	$(".bartender-question").append().html("<p>Do ye like it with a salty tang?</p>");
+	$(".bartender-question").append().html("<span class='flow-text'><a class='waves-effect waves-light btn yes-tang'>Yes</a><a class='waves-effect waves-light btn no-tang'>No</a></span>");
 	$(".yes-tang").click(function(){
 		tang = true;		
 	});
 };
 
-$(document).ready ( function() {
+	var Ingredients = function(){
+		this.strong = ['Glug of rum', 'slug of whisky', 'splash of gin'];
+		this.salty = ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon'];
+		this.bitter = ['Shake of bitters', 'splash of tonic', 'twist of lemon peel'];
+	
+	};
+
+	Ingredients.prototype.showMyDrink = function(strong,salty,bitter){
+		
+		
+		
+	};
+	
+	var Questions = function() {
+		this.strong = '<p>Do ye like yer drinks strong?</p>';
+		this.salty = '<p>Do ye like it with a salty tang?</p>';
+		this.bitter = '<p>Are ye a lubber who likes it bitter?</p>';
+		
+		
+	};
+	
+	Questions.prototype.showQuestion = function() {
+			var questionObject = this;
+			var choices = ['strong','salty','bitter'];
+			Object.keys(this).forEach(function(value,index){
+				 console.log(value);
+				 console.log(index);
+				
+				var question = questionObject[value];
+				console.log(question);
+				$(".bartender-question").append(question);
+				$(".bartender-question").append("<span class='flow-text'><a class='waves-effect waves-light btn yes-"+choices[index]+"'>Yes</a><a class='waves-effect waves-light btn no-"+choices[index]+"'>No</a></span>");
+			});
+				console.log(Object.keys(this));
+			  // $("bartender-question").append().html();
+			  
+			$(".yes-strong").click(function(){
+				strong = true;		
+			});
+			$(".yes-salty").click(function(){
+				salty = true;		
+			});
+			$(".yes-bitter").click(function(){
+				bitter = true;		
+			});
+			$(".no-strong").click(function(){
+				strong = false;		
+			});
+			$(".no-salty").click(function(){
+				salty = false;		
+			});
+			$(".no-bitter").click(function(){
+				bitter = false;		
+			});
+		};
+		
+
+		
+		
+
+	
+	var Ingredients = function () {
+	 this.strong = ['Glug of rum', 'slug of whisky', 'splash of gin'];
+	 this.salty = ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon'];
+	 this.bitter = ['Shake of bitters', 'splash of tonic', 'twist of lemon peel'];
+	 
+		
+		
+		
+	};
+
+$(document).ready(function(){
 	
 
-		$(".new-drink").click(function(){
+		// $(".new-drink").click(function(){
 			
-			var newDrink = new Bartender();
+			// var newDrink = new Bartender();
 			
-		});
-	
+		// });
+		
+		var questionList = new Questions();	
+		questionList.showQuestion();
 		
 		return false;
-
-
-// Musician.prototype.solo = function(length) {
-    // var solo = "";
-    // for (var i=0; i<length; i++) {
-        // solo += this.sounds[i % this.sounds.length] + " ";
-    // }
-    // console.log(solo);
-// };
-
-// var Guitarist = function() {
-    // Musician.call(this, ['Twang', 'Thrumb', 'Bling']);
-    // this.strings = 6;
-// };
-// Guitarist.prototype = Object.create(Musician.prototype);
-// Guitarist.prototype.constructor = Guitarist;
-
-// Guitarist.prototype.tune = function() {
-    // console.log('Be with you in a moment');
-    // console.log('Twoning sproing splang');
-// };
-
-// var Bassist = function() {
-    // Musician.call(this, ['Boink', 'Bow', 'Boom']);
-    // this.strings = 4;
-// };
-// Bassist.prototype = Object.create(Musician.prototype);
-// Bassist.prototype.constructor = Bassist;
-
-// var nigel = new Guitarist();
-// nigel.tune();
-// nigel.solo(8);
-
-// var derek = new Bassist();
-// derek.solo(4);
-
-
 });
 
 
